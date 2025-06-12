3a3251fde885dc834b3a1bcf0dfab0ab85544a87
wafaamohamed474 <wfam22171@gmail.com>
2025-06-12 12:45:08 +0300

    fix vercel footer error

--- src/layouts/privateLayout/index.jsx
+++ src/layouts/privateLayout/index.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
- 
+
 import Navbar from "../../components/navbar/Navbar";
 import { isAuthenticated } from "../../services/authService";
 import { Navigate } from "react-router";
